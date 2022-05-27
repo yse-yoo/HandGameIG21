0da254daca0ac6baeb191b003e821b14a02ff192
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-05-27 09:55:07 +0900

    add window

--- src/game/Window.java
+++ src/game/Window.java
@@ -5,6 +5,14 @@ import javax.swing.JFrame;
 public class Window extends JFrame{
 
 	public void create() {
+		//フレームのサイズ
+		setBounds(300, 200, 300, 200);
 		
+		//ウィンドウを閉じた時の処理
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
+		//フレームを表示
+		setVisible(true);
 	}
 }
